8159c31e69935e5cfcb24d4396772c45c00c9e5d
Matt Stroud <lixingke06@gmail.com>
2020-07-24 16:15:48 -0700

    add search bar layout

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Container, Jumbotron, Button, InputGroup, InputGroupAddon, Label, Input } from "reactstrap";
+import styled from "styled-components";
+
+const SearchContainer = styled(Container)`
+  padding-top: 200px;
+`;
 
 function Search() {
   return (
-    "Search"
+    <React.Fragment>
+      <SearchContainer>
+        <Jumbotron>
+          <InputGroup>
+            <Input type="text" placeholder="Software Engineer..." />
+            <InputGroupAddon>
+              <Button color="warning">Search</Button>
+            </InputGroupAddon>
+          </InputGroup>
+        </Jumbotron>
+      </SearchContainer>
+    </React.Fragment >
   );
 }
 
