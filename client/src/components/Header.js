c1547a188e020a487ee4b0bfdc8aa4cacd07796d
Matt Stroud <lixingke06@gmail.com>
2020-07-24 10:57:11 -0700

    update navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,11 +10,20 @@ function Header() {
   return (
     <React.Fragment>
       <div>
-        <Navbar className="navbar-dark bg-dark">
+        <Navbar className="navbar-dark bg-dark" expand="md">
           <NavbarBrand>JobIO</NavbarBrand>
           <Nav className="mr-auto" navbar>
             <NavItem>
-              <NavLink href="" onClick={() => stuff()}>Test</NavLink>
+              <NavLink href="">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">Dashboard</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">Applications</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">Search</NavLink>
             </NavItem>
           </Nav>
         </Navbar>
