45093461c27620e8455dd962c4c8bf0e6bef34c4
Matt Stroud <lixingke06@gmail.com>
2020-07-24 09:11:19 -0700

    add Splash and PageControl

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import Splash from "./Splash";
+import PageControl from "./PageControl";
 
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <Splash />
       <Footer />
     </React.Fragment>
   );
