999025ca63be2994a07b85625e88d07593b6d0e6
Matt Stroud <lixingke06@gmail.com>
2020-07-24 10:24:04 -0700

    add styled components

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,8 +1,37 @@
 import React from "react";
+import styled from "styled-components";
+import { Jumbotron, Button } from "reactstrap";
 
 function Splash() {
+
+  const Hero = styled(Jumbotron)`
+    background-color: #303f9f;
+    width: 100%;
+    border-radius: 0;
+    min-height: 50vh;
+    margin: 0;
+    text-align: center;
+    color: white;
+  `;
+
+  const Info = styled(Jumbotron)`
+    background-color: #303f9f;
+    border-radius: 0;
+    width: 100%;
+    margin: 0;
+  `;
+
   return (
-    "Splash"
+    <React.Fragment>
+      <Hero>
+        <h1>JOB APPLICATIONS MADE EASY</h1>
+        <p>Search for jobs and manage your applications.</p>
+        <Button color="warning">Call To Action Here</Button>
+      </Hero>
+      <Info>
+        Test
+      </Info>
+    </React.Fragment>
   );
 }
 
