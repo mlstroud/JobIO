0a03c53f9e9aaea8ebcdbef98bab85a0f67c1a87
Matt Stroud <lixingke06@gmail.com>
2020-07-24 09:54:27 -0700

    add basic navbar

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,25 @@
 import React from "react";
+import { Navbar, Nav, NavbarBrand, NavItem, NavLink } from "reactstrap";
 
 function Header() {
+
+  function stuff() {
+    alert("yaya");
+  }
+
   return (
-    "Header"
+    <React.Fragment>
+      <div>
+        <Navbar className="navbar-dark bg-dark">
+          <NavbarBrand>JobIO</NavbarBrand>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="" onClick={() => stuff()}>Test</NavLink>
+            </NavItem>
+          </Nav>
+        </Navbar>
+      </div>
+    </React.Fragment>
   );
 }
 
