95391d1d66629a9a3c97c84b4dfacb93f9868987
Matt Stroud <lixingke06@gmail.com>
2020-07-24 16:50:18 -0700

    add axios

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,12 +1,22 @@
 import React from "react";
 import { Container, Jumbotron, Button, InputGroup, InputGroupAddon, Label, Input } from "reactstrap";
 import styled from "styled-components";
+import { useState } from "react";
 
 const SearchContainer = styled(Container)`
   padding-top: 200px;
 `;
 
 function Search() {
+
+  const [stuff, doStuff] = useState(null);
+  let searchResults = [];
+
+  function getSearchResults() {
+
+    doStuff("lol");
+  }
+
   return (
     <React.Fragment>
       <SearchContainer>
@@ -15,10 +25,11 @@ function Search() {
           <InputGroup>
             <Input type="text" placeholder="Software Engineer..." />
             <InputGroupAddon>
-              <Button color="warning">Search</Button>
+              <Button color="warning" onClick={() => getSearchResults()}>Search</Button>
             </InputGroupAddon>
           </InputGroup>
         </Jumbotron>
+        {stuff}
       </SearchContainer>
     </React.Fragment >
   );
