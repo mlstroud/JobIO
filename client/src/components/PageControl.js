1f1d60e9559a2c3ef875a27da8c9eed85dec19e9
Matt Stroud <lixingke06@gmail.com>
2020-07-24 09:06:23 -0700

    add PageControl component

---
+++ client/src/components/PageControl.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class PageControl extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      "PageControl"
+    );
+  }
+}
+
+export default PageControl;
