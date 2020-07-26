a87dc0335a9dec82e06a4f18f3e7919c30b80cd6
Matt Stroud <lixingke06@gmail.com>
2020-07-25 18:21:30 -0700

    add component

---
+++ client/src/components/Job.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Job() {
+  return (
+    "Job"
+  );
+}
+
+export default Job;
---
+++ client/src/components/JobList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function JobList() {
+  return (
+    "JobList"
+  );
+}
+
+export default JobList;
