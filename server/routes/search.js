4e568d2cae8e32a100766e1d4aa3e5c1bcd9a3d1
Matt Stroud <lixingke06@gmail.com>
2020-07-25 23:39:08 -0700

    update to scrape basic data from generic indeed.com query

--- server/routes/search.js
+++ server/routes/search.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
+const cheerio = require('cheerio');
 
 /* GET search query and perform web scrape */
-router.get('/', function (req, res, next) {
-  res.send("test");
+router.get('/', async function (req, res, next) {
+  //const result = await axios.get("http://example.com");
+  const result = await axios.get(`https://www.indeed.com/jobs?q=Software+Engineer&l=`);
+  const $ = await cheerio.load(result.data);
+
+  // let ans = "";
+  // $(".jobsearch-SerpJobCard").each((i, elem) => {
+  //   ans += $(elem).find(".title").text() + "<br>" + $(elem).find(".summary").text() + "<br>";
+  // })
+  // res.send(ans);
+
+  let scrapeData = [];
+
+  $(".jobsearch-SerpJobCard").each((i, elem) => {
+    scrapeData.push({
+      title: $(elem).find(".title").text(),
+      company: $(elem).find(".company").text(),
+      location: $(elem).find(".location").text(),
+      summary: $(elem).find(".summary").text()
+    });
+  })
+
+  res.send(scrapeData);
 });
 
 module.exports = router;
