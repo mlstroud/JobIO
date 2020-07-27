47d14f7f64f071aa8b2caf25931de87fb9ceaceb
Matt Stroud <lixingke06@gmail.com>
2020-07-27 09:38:33 -0700

    add basic styling

--- client/src/components/Applications.js
+++ client/src/components/Applications.js
@@ -1,11 +1,23 @@
 import React from "react";
 import App from "./App";
 import ApplicationList from "./ApplicationList";
+import { Container, Jumbotron } from "reactstrap";
+import styled from "styled-components";
 
 function Applications(props) {
+
+  const ApplicationsContainer = styled(Container)`
+    padding-top: 200px;
+  `;
+
   return (
     <React.Fragment>
-      <ApplicationList />
+      <ApplicationsContainer>
+        <Jumbotron>
+          <h1>Applications</h1>
+        </Jumbotron>
+        <ApplicationList />
+      </ApplicationsContainer>
     </React.Fragment>
   );
 }
