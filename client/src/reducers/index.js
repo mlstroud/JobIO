2ae488c0d670e1a4d91e90855990dbbbfb2f6ac1
Matt Stroud <lixingke06@gmail.com>
2020-07-27 09:35:47 -0700

    add reducer for viewing apps

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,11 +2,13 @@ import manageApplicationReducer from "./manage-application-reducer";
 import searchReducer from "./searching-reducer";
 import { firestoreReducer } from "redux-firestore";
 import { combineReducers } from "redux";
+import viewApplicationsReducer from "./view-applications-reducer";
 
 const rootReducer = combineReducers({
   firestore: firestoreReducer,
   isSearching: searchReducer,
-  isEditing: manageApplicationReducer
+  isEditing: manageApplicationReducer,
+  viewingApplications: viewApplicationsReducer
 });
 
 export default rootReducer;
