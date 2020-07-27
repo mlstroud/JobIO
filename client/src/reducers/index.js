986a7a61c6486dc699ff8b1de9ecb8f5c9c29957
Matt Stroud <lixingke06@gmail.com>
2020-07-27 08:46:30 -0700

    add firestore reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import manageApplicationReducer from "./manage-application-reducer";
 import searchReducer from "./searching-reducer";
+import { firestoreReducer } from "redux-firestore";
 import { combineReducers } from "redux";
 
 const rootReducer = combineReducers({
+  firestore: firestoreReducer,
   isSearching: searchReducer,
   isEditing: manageApplicationReducer
 });
