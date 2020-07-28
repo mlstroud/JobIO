016db7ef430fcc2e6e3c13c5d73b8582f47836fc
Matt Stroud <lixingke06@gmail.com>
2020-07-27 17:44:24 -0700

    add sign in functionality and redirect to dashboard on success

--- client/src/components/SignIn.js
+++ client/src/components/SignIn.js
@@ -1,12 +1,47 @@
 import React from "react";
 import firebase from "firebase/app";
+import { Form, Button, Input } from "reactstrap";
+import { useState } from "react";
+import { Redirect } from "react-router-dom";
 
 function SignIn() {
-  return (
-    <React.Fragment>
-      <h1>Sign In</h1>
-    </React.Fragment>
-  );
+
+  const [userSignedIn, updateUserSignedIn] = useState(false);
+
+  function signInUser(event) {
+    event.preventDefault();
+    const email = event.target.email.value;
+    const password = event.target.password.value;
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+        updateUserSignedIn(true);
+        console.log("Signed in successfully");
+      }).catch((error) => {
+        console.log(error.message);
+      });
+  }
+
+  if (userSignedIn) {
+    return <Redirect to="/dashboard" />
+  } else {
+    return (
+      <React.Fragment>
+        <h1>Sign In</h1>
+        <Form onSubmit={signInUser}>
+          <Input
+            type="text"
+            name="email"
+            placeholder="Email" />
+          <Input
+            type="password"
+            name="password"
+            placeholder="Password" />
+          <Button type="submit" color="warning">Sign In</Button>
+        </Form>
+      </React.Fragment>
+    );
+  }
 }
 
 export default SignIn;
